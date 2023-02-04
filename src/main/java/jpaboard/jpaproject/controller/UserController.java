1087426e78547e541143993e21ef6ee80be82d85
서창희 <>
2023-02-04 16:42:59 +0900

    [Feature] 회원 가입, 로그인 로직 작성

--- src/main/java/jpaboard/jpaproject/controller/UserController.java
+++ src/main/java/jpaboard/jpaproject/controller/UserController.java
@@ -1,22 +1,25 @@
 package jpaboard.jpaproject.controller;
 
 import jpaboard.jpaproject.domain.User;
-import jpaboard.jpaproject.dto.UserRequestDto;
+import jpaboard.jpaproject.dto.UserJoinRequestDto;
+import jpaboard.jpaproject.dto.UserLoginRequestDto;
 import jpaboard.jpaproject.dto.UserResponseDto;
 import jpaboard.jpaproject.service.UserService;
+import jpaboard.jpaproject.utils.SessionUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
 
@@ -43,31 +46,79 @@ public class UserController {
     }
 
     /*
-    *   회원 가입 Form 띄우기
+    *   회원가입 Form 띄우기
     *   @param Model
-    *   @param UserRequestDto
+    *   @param UserJoinRequestDto
     *   @return String(View-Name)
     * */
     @GetMapping("/users/joinForm")
     public String userJoinForm(Model model) {
-        model.addAttribute("userRequestDto", new UserRequestDto());
-        return "";
+        model.addAttribute("userJoinRequestDto", new UserJoinRequestDto());
+        return "joinForm";
     }
 
     /*
+    *   로그인 Form 띄우기
+    *   @param Model
+    *   @return String(View-Name)
+    * */
+    @GetMapping("/users/loginForm")
+    public String userLoginForm(Model model) {
+        model.addAttribute("userLoginRequestDto", new UserLoginRequestDto());
+        return "loginForm";
+    }
+
+    /*
+    *   로그인
+    *   @param UserLoginRequestDto
+    *   @param HttpServletResponse
+    *   @return String(View-Name)
+    * */
+    @PostMapping("/users/login")
+    public String userLogin(@Valid UserLoginRequestDto userLoginRequestDto, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes) {
+
+        // 유효성 검사후 입력된 필드에 문제가 있을시 로그인 페이지로 다시 이동
+        if(bindingResult.hasErrors()) {
+            return "loginForm";
+        }
+
+        // 입력된 아이디와 패스워드가 회원정보와 일치하는지 조회
+        boolean isAuthorization = userService.isAuthorization(userLoginRequestDto);
+
+        // 입력된 정보가 일치하면 -> 메인페이지 이동
+        if(isAuthorization) {
+            // 로그인한 유저의 정보를 전달한다.
+            SessionUtils.addUserAttribute(userLoginRequestDto);
+
+            // 메인페이지 이동
+            return "redirect:/";
+
+            // 일치하지 않으면 -> 로그인 페이지로 redirect
+        } else {
+            redirectAttributes.addFlashAttribute("error_msg", "아이디 또는 비밀번호가 잘못되었습니다");
+            return "redirect:/users/loginForm";
+        }
+
+
+    }
+
+
+    /*
      *   회원 가입
-     *   @param UserRequestDto
-     *   @return ResponseEntity
+     *   @param UserJoinRequestDto
+     *   @return
      * */
     @PostMapping("/users")
-    public String joinUser(@Valid UserRequestDto userRequestDto, BindingResult bindingResult) {
+    public String joinUser(@Valid UserJoinRequestDto userJoinRequestDto, BindingResult bindingResult) {
 
+        // 입력한 필드에 유효성을 위반한 항목이있다면 회원가입 페이지로 다시 이동
         if(bindingResult.hasErrors()) {
-            return "";
+            return "joinForm";
         }
 
         // DTO => Entity 변환
-        User joinUser = userRequestDto.userRequestToEntity();
+        User joinUser = userJoinRequestDto.userRequestToEntity();
 
         // 회원 가입진행
         User userResponse = userService.join(joinUser);
@@ -88,27 +139,28 @@ public class UserController {
         User user = userService.findOneUser(userNo);
 
         // Entity => DTO 변환
-        UserRequestDto userRequestDto = new UserRequestDto(user);
+        UserJoinRequestDto userJoinRequestDto = new UserJoinRequestDto(user);
 
-        model.addAttribute("userRequestDto", userRequestDto);
+        model.addAttribute("userRequestDto", userJoinRequestDto);
 
         return "";
     }
 
     /*
      *   회원 수정
-     *   @param UserRequestDto
+     *   @param UserJoinRequestDto
      *   @return ResponseEntity
      * */
     @PatchMapping ("/users/{userNo}")
-    public String modifyUser(@Valid UserRequestDto userRequestDto, BindingResult bindingResult) {
+    public String modifyUser(@PathVariable("userNo") Long userNo,
+                             @Valid UserJoinRequestDto userJoinRequestDto, BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()) {
             return "";
         }
 
         // DTO => Entity 변환
-        User modifyUser = userRequestDto.userRequestToEntity();
+        User modifyUser = userJoinRequestDto.userRequestToEntity();
 
          // 회원정보 수정 진행
         User modifiedUser = userService.modifyUser(modifyUser);
@@ -128,7 +180,7 @@ public class UserController {
 
         model.addAttribute("userList", userResponseDtoList);
 
-        return "";
+        return "userList";
     }
 
     @GetMapping("user/delete")
