241fc792acf84eafc6d579040415622fd5ff7cfe
서창희 <>
2023-02-05 19:36:42 +0900

    [Feature] 세션 끊기 로직 및 로그인 중 회원 아이디 반환 로직 추가

--- src/main/java/jpaboard/jpaproject/utils/SessionUtils.java
+++ src/main/java/jpaboard/jpaproject/utils/SessionUtils.java
@@ -9,13 +9,58 @@ import javax.servlet.http.HttpSession;
 public class SessionUtils {
 
 
+    /*
+    *   회원 로그인시 Session 등록
+    *   @param UserLoginRequestDto
+    * */
     public static void addUserAttribute(UserLoginRequestDto dto) {
 
         // Session을 얻어온다.
-        HttpSession session = (HttpSession)((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+        HttpSession session = getSession();
 
         // Session에 회원 정보 등록
         session.setAttribute("loginUser", dto);
 
     }
+
+    /*
+    *   Session에 등록된 회원 아이디를 반환 - (등록자 & 수정자ID 셋팅용)
+    *   @return String(userId)
+    * */
+    public static String getUserId() {
+
+        // Session을 얻어온다.
+        HttpSession session = getSession();
+
+        // Session에서 로그인한 회원의 정보를 얻어온다.
+        UserLoginRequestDto userDto = (UserLoginRequestDto)session.getAttribute("loginUser");
+
+        // User의 객체에서 ID만 반환한다.
+        return userDto.getId();
+    }
+
+
+
+    /*
+    *   로그아웃시 사용할 세션 끊기 메서드
+    * */
+    public static void invalidateSession() {
+
+        // Session을 얻어온다.
+        HttpSession session = getSession();
+
+        // Session을 끊는다.
+        session.invalidate();
+    }
+
+
+    /*
+    *   Session을 얻어오는 로직
+    *   @return HttpSession
+    * */
+    public static HttpSession getSession() {
+        return (HttpSession) ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+    }
+
+
 }
